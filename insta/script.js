d1974a11e6ca681ac710bb02cf8c46d5b11f4b37
Main Projects(KAmarJahan) <95184009+futrengine@users.noreply.github.com>
2025-06-27 21:52:50 +0530

    Update script.js

--- insta/script.js
+++ insta/script.js
@@ -10,7 +10,7 @@ async function download() {
   output.innerHTML = "⏳ Fetching download link...";
 
   try {
-    const res = await fetch("https://3ef2889e-1afd-4f09-b501-ebbed725ecfd-00-2oh5jrkyumor0.sisko.replit.dev/getInstaMedia", {
+    const res = await fetch("https://insta-backend.onrender.com/getInstaMedia", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
